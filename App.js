796f80fcab092bc763c7c57fdc298385a115c459
Rokas <rookas.rudzenskas@gmail.com>
2022-12-10 10:46:50 +0200

    Native wind setup

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+    const [gender, setGender] = useState('man');
+    const [age, setAge] = useState(30);
+    const [priceMin, setPriceMin] = useState(25);
+    const [priceMax, setPriceMax] = useState(100);
+    const [hobbies, setHobbies] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const [result, setResult] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
